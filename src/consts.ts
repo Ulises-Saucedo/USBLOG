b6957268e6711d6aa41b0e3991f5ce839e90b247
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2024-06-16 19:46:11 -0300

    translated to es

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "USBLOG";
-export const SITE_DESCRIPTION = "Welcome to USBLOG";
+export const SITE_DESCRIPTION = "Bienvenido a USBLOG";
